f206e1362c8d7eba14990a231c971b73fcfcd934
yassirErr <yassirerrasmouki@gmail.com>
2022-07-20 00:00:35 +0100

    End of section 2

--- API/Startup.cs
+++ API/Startup.cs
@@ -42,6 +42,12 @@ namespace API
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
 
             });
+            services.AddCors(opt =>{
+                 opt.AddPolicy("CorsPolicy", policy =>
+                    {
+                     policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");  
+                    });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -57,7 +63,7 @@ namespace API
             // app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseCors("CorsPolicy");
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>

ae4d092906f4eab81297878ca5c6be8c20cea2de
yassirErr <yassirerrasmouki@gmail.com>
2022-07-19 12:17:56 +0100

    first commit

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+
+    [Route("api/[controller]")] // api/activities
+
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
---
+++ API/Program.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            // this function checking and create new database based on migration *if database not created*
+           var host = CreateHostBuilder(args).Build();
+            using var scope = host.Services.CreateScope();
+            var service = scope.ServiceProvider;
+            try 
+	        {	        
+		        var context = service.GetRequiredService<DataContext>();
+                await  context.Database.MigrateAsync();
+                await Seed.SeedData(context);
+	        }
+	        catch (Exception Ex)
+	        {
+                var logger = service.GetRequiredService<ILogger<Program>>();
+                logger.LogError(Ex,"An Error occured during migration");
+	        }
+            await host.RunAsync();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
